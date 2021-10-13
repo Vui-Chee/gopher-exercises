167be85779261677226e1e66c6fd2d8411e9f09e
Vui-Chee <vuicheesiew@gmail.com>
2021-10-13 20:46:04 +0800

    Test insert stuff into postgres

--- phone-number-normalizer/main.go
+++ phone-number-normalizer/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 	"strings"
 	"unicode"
+
+	_ "github.com/lib/pq"
 )
 
 func normalize(phone string) string {
@@ -18,4 +24,36 @@ func normalize(phone string) string {
 }
 
 func main() {
+	if len(os.Args) <= 1 {
+		log.Println("Please supply a single word.")
+		return
+	}
+	word := os.Args[1]
+
+	connStr := "dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	_, err = db.Exec(fmt.Sprintf("insert into users values ('%s')", word))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if rows, err := db.Query("SELECT * FROM users"); err == nil {
+		for rows.Next() {
+			var (
+				name string
+			)
+			if err := rows.Scan(&name); err != nil {
+				log.Fatal(err)
+			}
+			println(name)
+		}
+	} else {
+		log.Fatal("Query failed, reason: ", err)
+	}
 }
