703ff881f87a119487f46c1b1ef9b0442c1597d2
Vui-Chee <vuicheesiew@gmail.com>
2021-10-15 08:12:01 +0800

    Refactor

--- phone-number-normalizer/main.go
+++ phone-number-normalizer/main.go
@@ -11,6 +11,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var pool *sql.DB
+
 func normalize(phone string) string {
 	var builder strings.Builder
 
@@ -24,36 +26,41 @@ func normalize(phone string) string {
 }
 
 func main() {
+	// Get arg
 	if len(os.Args) <= 1 {
 		log.Println("Please supply a single word.")
 		return
 	}
 	word := os.Args[1]
 
+	// Try connect
 	connStr := "dbname=postgres sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	pool, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer pool.Close()
+
+	// Insert db
+	_, err = pool.Exec(fmt.Sprintf("insert into users values ('%s')", word))
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
-	_, err = db.Exec(fmt.Sprintf("insert into users values ('%s')", word))
+	// Query db
+	rows, err := pool.Query("SELECT * FROM users")
 	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	if rows, err := db.Query("SELECT * FROM users"); err == nil {
-		for rows.Next() {
-			var (
-				name string
-			)
-			if err := rows.Scan(&name); err != nil {
-				log.Fatal(err)
-			}
-			println(name)
-		}
-	} else {
 		log.Fatal("Query failed, reason: ", err)
+		return
+	}
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatal(err)
+		}
+		println(name)
 	}
 }
