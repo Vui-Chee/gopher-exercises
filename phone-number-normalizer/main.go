744bb36d86612f5dac7c65b7692c8634eea131c9
Vui-Chee <vuicheesiew@gmail.com>
2021-10-17 14:42:51 +0800

    Try insert bunch of numbers into table

--- phone-number-normalizer/main.go
+++ phone-number-normalizer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -67,7 +68,32 @@ func createTable(conn *sql.DB, tableName string, tableSchema string) error {
 	return nil
 }
 
+func insertTable(conn *sql.DB, tableName string, values []string) error {
+	if conn == nil {
+		return errors.New("Please provide a database to insert into.")
+	}
+
+	// For simplicity, I am not going to expolate to any kind of table.
+	for _, value := range values {
+		_, err := conn.Exec(fmt.Sprintf("insert into %s values (default, '%s')", tableName, value))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func main() {
+	phone_numbers := []string{
+		"1234567890",
+		"123 456 7891",
+		"(123) 456 7892",
+		"(123) 456-7893",
+		"123-456-7894",
+		"(123)456-7892",
+	}
+
 	scream(createDB(DB_NAME))
 	conn, err := connectDB(DB_NAME) // Use the newly created database
 	scream(err)
@@ -75,4 +101,5 @@ func main() {
 		id SERIAL PRIMARY KEY,
 		number VARCHAR(255)
 	`))
+	scream(insertTable(conn, "phone_numbers", phone_numbers))
 }
